a156aa9ca9628f2f49cba878753426930554e3e2
Dnyaneshwar Yadav Wagh <85550894+dnyaneshwarwagh0072@users.noreply.github.com>
2024-12-08 18:04:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 function getFormvalue() {
-    //Write your code here
-
+  const firstName = document.getElementById("firstName").value;
+  const lastName = document.getElementById("lastName").value;
+  alert(`${firstName} ${lastName}`);
 }
